3200ff9969e470bf1f19063e5c684f3698299655
FlipperRage <flipper.rage@gmail.com>
2025-02-23 02:55:30 +0800

    feat: add player-asteroid collision check
    - implement collision detection between player and asteroids

--- circleshape.py
+++ circleshape.py
@@ -20,3 +20,9 @@ class CircleShape(pygame.sprite.Sprite):
     def update(self, dt):
         # sub-classes must override
         pass
+    
+    def collision_check(self, target):
+        distance = pygame.math.Vector2.distance_to(self.position, target.position)
+        if distance <= (self.radius + target.radius):
+            return True
+        return False
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygame
+import sys
 from constants import *
 from player import Player
 from asteroid import Asteroid
@@ -36,6 +37,11 @@ def main():
 
         for obj in drawable:
             obj.draw(screen)
+        
+        for asteroid in asteroids:
+            if player.collision_check(asteroid):
+                print("Game Over!")
+                sys.exit()
         
         pygame.display.flip()
         dt = pygameClock.tick(60) / 1000
