df6c44a2f58988e325a7a14f9ff74f303ab5140c
FlipperRage <flipper.rage@gmail.com>
2025-02-23 01:49:28 +0800

    feat: add player sprite with movement controls
    - create player sprite entity
    - enable rotation using 'a' and 'd' keys
    - implement forward/backward movement with 'w' and 's' keys

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygame
+import player
 from constants import *
 
 
@@ -7,20 +8,31 @@ def main():
     pygame.init()
 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    pygameClock = pygame.time.Clock()
+
+    dt = 0
+    x = SCREEN_WIDTH / 2
+    y = SCREEN_HEIGHT / 2
+
+    current_player = player.Player(x, y)
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-                
+        
         screen.fill((0, 0, 0))
-
+        current_player.draw(screen)
         pygame.display.flip()
+        dt = pygameClock.tick(60) / 1000
 
+        current_player.update(dt)
+
+    """
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
-
+    """
 
 
 
